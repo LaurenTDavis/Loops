84a315879a130c43d496d5d576fb22d5c6e923a0
Student <student@RU-009.local>
2015-09-14 14:04:14 -0600

    loop1 and loop2

---
+++ main.js
@@ -0,0 +1,115 @@
+// for (var i = 0; i <= 500; i += 100) {
+// 	console.log(i);
+// }
+
+// for (var i = 1; i <= 64; i *= 2) {
+// 	console.log(i);
+// }
+
+// for (var i = 1; i < 4; i++) {
+// 	console.log(i);
+// 	console.log(i);
+// 	console.log(i);
+// }
+
+// for (var i = 0; i < 11; i += 2) {
+// 	console.log(i);
+// }
+
+// for (var i = 0; i < 16; i += 3) {
+// 	console.log(i);
+// }
+
+// for (var i = 9; i >= 0; i--) {
+// 	console.log(i);
+// }
+
+// for (var i = 0; i < 3; i) {
+// 	console.log(0);
+// 	console.log(1);
+// 	console.log(2);
+// 	console.log(3);
+
+// }
+
+// var animals = ['rat', 'cat', 'butterfly', 'marmot', 'ocelot']; 
+
+// 1. 
+// for (var i = 0; i < animals.length - 1; i++) {
+// 	console.log(animals[i]);
+// }
+
+// 2.
+// for (var i = 0; i < animals.length; i += 2) {
+// 	console.log(animals[i]);
+// }
+
+// 3.
+// for (var i = animals.length-1; i >= 0; i--) {
+// 	console.log(animals[i]);
+// }
+
+// 4.
+// for (var i = 0; i < animals.length-1; i++) {
+// 	console.log(animals[i]); 
+// 	console.log(animals[i + 1]); 
+// }
+
+
+
+
+var students = [{
+ 	name: 'Liz',
+ 	age: 25,
+ 	city: 'Boulder'
+},{
+	name: 'Meghan',
+ 	age: 27,
+ 	city: 'Denver'
+},{
+ 	name: 'Trent',
+ 	age: 32,
+ 	city: 'Boulder'
+},{
+ 	name: 'Chelsea',
+ 	age: 24,
+ 	city: 'Boulder'
+},{
+ 	name: 'Amir',
+ 	age: 18,
+ 	city: 'Denver'
+}];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
